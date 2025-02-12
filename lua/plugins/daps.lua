43e8e895484abc57f2fcdb490ee82100916cf4e2
Adam <pasekaadam@gmail.com>
2025-02-12 19:48:52 +0100

    edit file layout, add cpp dap support

---
+++ lua/plugins/daps.lua
@@ -0,0 +1,5 @@
+-- List of downloaded DAPs
+
+return {
+  'codelldb',
+}
---
+++ lua/plugins/linters.lua
@@ -0,0 +1,12 @@
+-- List of current linters
+
+return {
+  markdown = { 'markdownlint' },
+  cpp = { 'cpplint' },
+  c = { 'cpplint' },
+  python = { 'pylint' },
+  docker = { 'hadolint' },
+  bash = { 'shellcheck' },
+  cmake = { 'cmakelang' },
+  lua = { 'luacheck' },
+}
